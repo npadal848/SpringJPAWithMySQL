c9640ce74e030f74f81e7c4ee43bc99243efb419
Nageswar Padal <npadal848@gmail.com>
2021-04-23 22:45:44 +0530

    initial setup

---
+++ src/main/java/com/np/jpa/SpringJpaWithMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.np.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaWithMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaWithMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/np/jpa/SpringJpaWithMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaWithMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
